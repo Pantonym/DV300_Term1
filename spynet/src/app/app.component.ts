d1329074e2501256e7cdb1ee29497491ad0e5ffb
Pantonym <221179@virtualwindow.co.za>
2024-02-23 13:51:41 +0200

    Routing

--- spynet/src/app/app.component.ts
+++ spynet/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// Toolbar imports
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- spynet/src/app/app.routes.ts
+++ spynet/src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { LoginComponent } from './component/login/login.component';
+import { MarketplaceComponent } from './component/marketplace/marketplace.component';
+import { WarehousesComponent } from './component/warehouses/warehouses.component';
+import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
+    { path: "dashboard", component: DashboardComponent },
+    { path: "login", component: LoginComponent },
+    { path: "marketplace", component: MarketplaceComponent },
+    { path: "warehouses", component: WarehousesComponent },
+    { path: "", redirectTo: "dashboard", pathMatch: "full" },
+    { path: "**", component: PagenotfoundComponent }
 ];
