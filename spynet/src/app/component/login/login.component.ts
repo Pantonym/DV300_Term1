f19f54fd1c1f107e6758748853540de31ce2bf08
Pantonym <221179@virtualwindow.co.za>
2024-02-25 11:10:50 +0200

    progress on sign in and warehouses pages

--- spynet/src/app/component/login/login.component.ts
+++ spynet/src/app/component/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatSliderModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
