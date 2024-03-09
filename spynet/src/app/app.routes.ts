f9c9cff7a9489be4131cc5e0e35cc2e97f13d31d
GlenDorner221358 <221358@virtualwindow.co.za>
2024-03-09 18:52:52 +0200

    added Marketplace page
    The front end for the marketplace and marketplace admin pages have been added

--- spynet/src/app/app.routes.ts
+++ spynet/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './component/login/login.component';
 import { MarketplaceComponent } from './component/marketplace/marketplace.component';
 import { WarehousesComponent } from './component/warehouses/warehouses.component';
 import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
+import { MarketplaceAdminComponent } from './component/marketplace-admin/marketplace-admin.component';
 
 export const routes: Routes = [
     { path: "dashboard", component: DashboardComponent },
     { path: "login", component: LoginComponent },
     { path: "marketplace", component: MarketplaceComponent },
+    { path: "marketplace-admin", component: MarketplaceAdminComponent },
     { path: "warehouses", component: WarehousesComponent },
     { path: "", redirectTo: "dashboard", pathMatch: "full" },
     { path: "**", component: PagenotfoundComponent }
---
+++ spynet/src/app/component/marketplace-admin/marketplace-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './marketplace-admin.component.html',
+  styleUrl: './marketplace-admin.component.css'
+})
+export class MarketplaceAdminComponent {
+
+}
