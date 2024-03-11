8cba69c989c23571f2059fe981dc15108eb5e151
Pantonym <221179@virtualwindow.co.za>
2024-03-11 16:15:56 +0200

    bootstrap, coreui installation

---
+++ spynet/src/app/component/carousel/carousel.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertModule } from '@coreui/angular';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/img/angular.jpg',
+    };
+    this.slides[1] = {
+      src: './assets/img/react.jpg',
+    }
+    this.slides[2] = {
+      src: './assets/img/vue.jpg',
+    }
+  }
+}
+
+// npm install @coreui/coreui
+// npm install @coreui/angular@4
+// npm install bootstrap
